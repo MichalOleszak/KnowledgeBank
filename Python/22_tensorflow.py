722c33f3523724fc2ea062d8b98692da96eb47cb
Michal Oleszak <michal.oleszak@cosmose.co>
2019-07-17 17:19:31 +0200

    [UPD] Updated Python Tensorflow script

--- Python/22_tensorflow.py
+++ Python/22_tensorflow.py
@@ -251,8 +251,25 @@ for j in range(0, 30000, 2000):
     var_list=[weights1, bias1, weights2, bias2])
 
 
+# Training models with the Estimators API -----------------------------------------------------------------------------
+# High-level API; less flexible; easier to deploy; contains ready-to-use models
 
+# Define feature columns for bedrooms and bathrooms
+bedrooms = feature_column.numeric_column("bedrooms")
+bathrooms = feature_column.numeric_column("bathrooms")
 
+# Define the list of feature columns
+feature_list = [bedrooms, bathrooms]
 
+def input_fn():
+  # Define the labels
+  labels = np.array(housing['price'])
+  # Define the features
+  features = {'bedrooms':np.array(housing['bedrooms']), 
+                'bathrooms':np.array(housing['bathrooms'])}
+  return features, labels
+
+model = estimator.DNNRegressor(feature_columns=feature_list, hidden_units=[2,2])
+model.train(input_fn, steps=1)
 
 
