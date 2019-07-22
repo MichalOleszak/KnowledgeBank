e3ea750b4af20fdaebce64a51f432c355785e7ae
Michal Oleszak <oleszak.michal@gmail.com>
2019-07-23 00:24:38 +0200

    [UPD] Convnets notes

--- Python/23_convolutional_nets.py
+++ Python/23_convolutional_nets.py
@@ -109,7 +109,69 @@ model.add(Conv2D(10, kernel_size=3, activation='relu',
 # S - stride
 
 
-# 
+# Pooling -------------------------------------------------------------------------------------------------------------
+
+# CNNs can have a lot of parameters. Pooling layers are often added between the convolutional layers of a neural 
+# network to summarize their outputs in a condensed manner, and reduce the number of parameters in the next layer 
+# in the network. This can help us if we want to train the network more rapidly, or if we don't have enough data 
+# to learn a very large number of parameters.
+# A pooling layer can be described as a particular kind of convolution. For every window in the input it finds 
+# the maximal pixel value and passes only this pixel through.
+
+# Result placeholder
+result = np.zeros((im.shape[0]//2, im.shape[1]//2))
+
+# Pooling operation
+for ii in range(result.shape[0]):
+    for jj in range(result.shape[1]):
+        result[ii, jj] = np.max(im[ii*2:ii*2+2, jj*2:jj*2+2])
+		
+
+# Tracking model's learning -------------------------------------------------------------------------------------------
+
+# Plot learning curves
+training = model.fit(train_data, train_labels, epochs=3, batch_size=10, validation_split=0.2)
+history = training.history
+plt.plot(history['loss'])
+plt.plot(history['val_loss'])
+plt.show()
+
+# Using stored weights to predict in a test set
+# Model weights stored in an hdf5 file can be reused to populate an untrained model. Once the weights are loaded 
+# into this model, it behaves just like a model that has been trained to reach these weights. 
+model.load_weights('weights.hdf5')
+model.predict(test_data) 
 
 
+# Regularization ------------------------------------------------------------------------------------------------------
 
+# Dropout is a form of regularization that removes a different random subset of the units in a layer in each 
+# round of training. In keras, dropout layer affects the preceding layer, so it should follow a conv layer.
+model.add(Dropout(0.2))
+
+# Batch normalization is another form of regularization that rescales the outputs of a layer to make sure that they 
+# have mean 0 and standard deviation 1.
+model.add(BatchNormalization())
+
+
+# Interpreting the model ----------------------------------------------------------------------------------------------
+
+# One way to interpret models is to examine the properties of the kernels in the convolutional layers.
+# Load the weights into the model
+model.load_weights('weights.hdf5')
+# Get the first convolutional layer from the model
+c1 = model.layers[0]
+# Get the weights of the first convolutional layer
+weights1 = c1.get_weights()
+# Pull out the first channel of the first kernel in the first layer
+# shape = [kernel_shape_rows, kernel_shape_cols, num_channels, num_kernels]
+kernel = weights1[0][:, :, 0, 0]
+
+# Visualizing kernel responses
+# One of the ways to interpret the weights of a neural network is to see how the kernels stored in these weights 
+# "see" the world. That is, what properties of an image are emphasized by this kernel.
+# Convolve with the fourth image in test_data
+out = convolution(test_data[3, :, :, 0], kernel)
+# Visualize the result
+plt.imshow(out)
+plt.show()
